f22eb3faa06b3e6f3566e648a5eec23282da95f0
Duha54rus <duha9898@gmail.com>
2020-05-03 17:16:54 +0300

    Implemented MIDI playback using pygame Also code refactoring

--- Player.py
+++ Player.py
@@ -1,21 +1,17 @@
-#from platform import system
-#if system() == 'Windows':
-#    import winsound
-#else:
-#    print('This software is currently only compatible with Windows OS')
-#    exit()
 from decimal import getcontext, Decimal
 from random import randint
 from miditime.miditime import MIDITime
+from pathlib import Path
 
 getcontext().prec = 60
+midifile = Path.home().joinpath('Desktop', 'irrational_music.mid')
 
 number = Decimal(input('Enter a positive number: '))
 bpm = int(input('Enter rhythm in BPM: '))
 velocity = int(input('Enter note velocity (loudness) 0-127: '))
 
 playstring = str(number.sqrt()).replace('.', '', 1)
-midiout = MIDITime(bpm, 'irrational_music.mid')
+midiout = MIDITime(bpm, midifile)
 notes = (68, 69, 71, 72, 74, 76, 77, 80, 81, 83)
 
 midinotes = []
@@ -29,6 +25,9 @@ for time in range(len(playstring)):
 
 midiout.add_track(midinotes)
 midiout.save_midi()
-#winsound.PlaySound('irrational_music.mid', winsound.SND_FILENAME)
-#TODO: Implement playback of the created .mid file.
-print('Find the resulting "irrational_music.mid" file in the program folder')
+print('You can find the resulting "irrational_music.mid" file at your desktop')
+
+from midi_playback import player_init
+player_init(midifile)
+
+print('Thank you for listening!')
